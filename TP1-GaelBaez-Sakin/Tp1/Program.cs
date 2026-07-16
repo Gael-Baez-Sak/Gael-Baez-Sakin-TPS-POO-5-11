416be1c39c5a03447a524e8463511c2d1d79d92c
Gael_Baez_Sak <gaelbaezsakin@gmail.com>
2026-07-15 23:59:51 -0300

    Add files via upload

--- TP1-GaelBaez-Sakin/Tp1/Program.cs
+++ TP1-GaelBaez-Sakin/Tp1/Program.cs
@@ -19,6 +19,7 @@ namespace Tp1
 
     class Configuracion
     {
+        // clase configuracion con los parametros del largo, ancho y velocidad en que caen los copos de nieve
         public int Filas = 15;
         public int Columnas = 10;
         public int Velocidad = 250;
@@ -26,9 +27,11 @@ namespace Tp1
 
     class Copo
     {
+        // clase copo con los parametro de las posiciones x (columna) e y (fila)
         public int X;
         public int Y;
 
+        // constructor de copo que recibe la posicion X y la posicion Y se inicializa en 0 pq los copos siempre aparecen arriba del todo
         public Copo(int x)
         {
             X = x;
@@ -38,79 +41,99 @@ namespace Tp1
 
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
+            // oculta el cursor para que se vea mas limpio todo
             Console.CursorVisible = false;
 
             Configuracion config = new Configuracion();
 
+            // lista de los copos cayendo 
             List<Copo> copos = new List<Copo>();
+
+            // Matriz para los copos ya apoyados
+            // si es false = vacío
+            // si es true = hay un copo
             bool[,] suelo = new bool[config.Filas, config.Columnas];
 
             Random random = new Random();
 
             while (true)
             {
-                // Crear un copo en una columna aleatoria
+                // poner un copo en una columna aleatoria
                 int columna = random.Next(config.Columnas);
                 if (!suelo[0, columna])
-                {   //si la primer fila de esa columna esa libre, añade un copo
+                {   // si la primer fila de esa columna esa libre, añade un copo
                     copos.Add(new Copo(columna));
                 }
 
-                // Mover copos
+                // mover copos
                 for (int i = copos.Count - 1; i >= 0; i--)
                 {
                     Copo c = copos[i];
 
+                    // se fija si el copo llego a la última fila
                     bool llegoAlFondo = c.Y == config.Filas - 1;
+
+                    // si todavia no llego al fondo, revisar si abajo hay un copo apoyado.
                     bool chocaConOtro = !llegoAlFondo && suelo[c.Y + 1, c.X];
 
+                    // si llegó al fondo o chocó con otro copo
                     if (llegoAlFondo || chocaConOtro)
                     {
+                        // queda fijo en el suelo
                         suelo[c.Y, c.X] = true;
+
+                        // ya no sigue cayendo y se elimina de la list
                         copos.RemoveAt(i);
                     }
                     else
                     {
+                        // si no choco, baja una fila
                         c.Y++;
                     }
                 }
 
-                // Eliminar filas completas
+                // borrar las filas completas
+                // revisar todas las filas desde abajo hacia arriba
                 for (int fila = config.Filas - 1; fila >= 0; fila--)
                 {
                     bool completa = true;
 
+                    // comprueba si toda la fila esta llena.
                     for (int x = 0; x < config.Columnas; x++)
                     {
                         if (!suelo[fila, x])
                         {
+                            // si hay un espacio vacio, la fila no esta completa
                             completa = false;
                             break;
                         }
                     }
 
+                    // si la fila esta completa, se borra la fila
                     if (completa)
                     {
                         for (int y = fila; y > 0; y--)
                         {
                             for (int x = 0; x < config.Columnas; x++)
                             {
+                                // todas las filas se mueven una fila hacia abajo, copiando la fila de arriba
                                 suelo[y, x] = suelo[y - 1, x];
                             }
                         }
 
+                        // la primera fila queda vacía porque no hay ninguna fila por encima para copiar
                         for (int x = 0; x < config.Columnas; x++)
                             suelo[0, x] = false;
 
-                        fila++; // volver a revisar la misma fila luego del desplazamiento
+                        fila++; // como las filas bajaron, vuelve a revisar la misma fila despues de mover
                     }
                 }
 
                 Console.Clear();
 
-                // Dibujar copos acumulados
+                // imprimir copos acumulados
                 for (int y = 0; y < config.Filas; y++)
                 {
                     for (int x = 0; x < config.Columnas; x++)
@@ -123,13 +146,14 @@ namespace Tp1
                     }
                 }
 
-                // Dibujar copos cayendo
+                // imprimir copos cayendo
                 foreach (Copo c in copos)
                 {
                     Console.SetCursorPosition(c.X, c.Y);
                     Console.Write("*");
                 }
 
+                // espera 250 milisegundos antes de la siguiente actualizacion
                 Thread.Sleep(config.Velocidad);
             }
         }
